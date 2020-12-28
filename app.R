ab1105439e378d6a55869dd606480fc3d03e4e41
Eduardo Vásquez <evasquezt@uc.cl>
2020-12-28 14:45:59 -0300

    repository initialization

---
+++ app.R
@@ -0,0 +1,15 @@
+library(shiny)
+
+
+# Define UI for application
+ui <- fluidPage(
+
+)
+
+# Define server logic
+server <- function(input, output) {
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
