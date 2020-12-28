2e6f4c57099646ee84c4890869cb11f11304d925
Eduardo Vásquez <evasquezt@uc.cl>
2020-12-28 16:30:10 -0300

    eyp2307 - UI de random walk

--- EYP2307-procesos_estocasticos/app.R
+++ EYP2307-procesos_estocasticos/app.R
@@ -1,11 +1,56 @@
 library(shiny)
+library(shinyFeedback)
 
+
+implementado <- c("Caminata aleatoria")
+
+# UI implementation
 ui <- fluidPage(
-  
+  titlePanel(strong("EYP2307 - Procesos Estocásticos")),
+  br(),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("select", "Visualización",
+                  choices = c("", implementado)),
+      br(),
+      uiOutput("parameters")
+    ),
+    mainPanel(
+      uiOutput("results")
+    )
+  )
 )
 
 server <- function(input, output, session) {
+  # Parameter inputs creator
+  parameters_ui <- reactive({
+    req(input$select)
+    if (input$select == "Caminata aleatoria"){
+      list(
+        numericInput("rw_prob", "p", min = 0, max = 1,
+                     step = 0.1, value = 0.5),
+        numericInput("rw_size", "n", min = 1, step = 1, value = 50),
+        # sliderInput("rw_time", "Tiempo", min = 0.01, max = 10, value = 0.5),
+        actionButton("rw_start", "Simular"),
+        br(),br(),
+        # actionButton("rw_stop", "Detener")
+      )
+    }
+  })
   
+  # Parameter inputs sender
+  output$parameters <- renderUI({
+    req(input$select)
+    tagList(
+      parameters_ui()
+    )
+  })
+  
+  # Random walk
+  observeEvent(input$rw_start, {
+    
+  })
 }
+
 
 shinyApp(ui, server)
